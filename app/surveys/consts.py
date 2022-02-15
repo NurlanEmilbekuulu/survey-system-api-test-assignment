caa84f9e6b755d984445a37f34f4f42b9d191ba2
Нурлан Эмильбеков <Nemilbekov@stat.kg>
2022-02-15 09:54:02 +0600

    the answer types for the questions have been declared

---
+++ app/surveys/consts.py
@@ -0,0 +1,11 @@
+""" For constants """
+
+TEXT = 'text'
+SINGLE_CHOICE = 'single'
+MULTIPLE_CHOICE = 'multiple'
+
+QUESTION_TYPES = (
+    (TEXT, 'text'),
+    (SINGLE_CHOICE, 'Select single'),
+    (MULTIPLE_CHOICE, 'Select multiple')
+)
